e7c907e78927255e8d89fefe057408cbc0bce884
TabithaKoech <tabbyk1718@gmail.com>
2022-06-05 20:54:10 +0300

    added a hover styling on the navigation links

---
+++ Assets/js/index.js
@@ -0,0 +1,7 @@
+function mouseOver() {
+    document.getElementById("contact").style.color = "#F0A500";
+  }
+  function mouseOut() {
+    document.getElementById("contact").style.color = "black";
+  }
+  
